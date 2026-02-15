ea7df74e95f3ce6aed2c6a66c52826527b2948ec
Sameer6556 <singhsameer6556@gmail.com>
2026-02-15 21:26:47 +0530

    otp issue on render

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -3,13 +3,11 @@ const nodemailer = require("nodemailer")
 const mailSender = async (email, title, body) => {
   try {
     let transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
+      service: "gmail",
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
-      secure: true,
-      port: 465,
     })
 
     let info = await transporter.sendMail({
